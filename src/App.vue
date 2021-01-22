2ba577f0523943bdda7610a9c719b1c39d75beeb
Danyar_uulu <67176587+Wenty-lyator@users.noreply.github.com>
2021-01-22 17:02:05 +0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+   <div id="app">
+      <div class="nav-wrapper">
+         <Nav></Nav>
+
+         <Navmob></Navmob>
+      </div>
+
+     <router-view></router-view>
+   </div>
+</template>
+
+<script>
+import Nav from './components/Nav'
+import Navmob from './components/Navmob'
+
+export default {
+  name: 'App',
+  components: {
+     Nav,
+     Navmob
+  }
+}
+</script>
